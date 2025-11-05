a05e3e7b654f9d78b0e700f2e4eebfcfbe8c30a2
Youssef Atef <youssef.atef2037@gmail.com>
2025-11-05 16:42:05 +0200

    create a new nodejs app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return 'Hello, $(name)!';
+
+
+}
+
+MediaSourceHandle.exports= greet;
+
+if(requestAnimationFrame.main ---module) {
+
+    console.log(greet("World"));
+    
+}
